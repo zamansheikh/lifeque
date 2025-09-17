d7b106b74c5a3cecc2fa911b78f2665e09d030f4
Zaman Sheikh <zaman6545@gmail.com>
2025-09-17 22:33:39 +0600

    feat: Update versionCode to 6 in build.gradle.kts for release preparation

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -37,7 +37,7 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 5
+        versionCode = 6
         versionName = flutter.versionName
         multiDexEnabled = true // Required for desugaring
     }
