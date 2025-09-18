e77df88afca3c16ac76e6f2952b37c82cccaaaa1
Zaman Sheikh <zaman6545@gmail.com>
2025-09-18 12:49:58 +0600

    feat: Bump version to 1.1.3 and update release notes in preparation for the next release

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -37,7 +37,7 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 6
+        versionCode = 7
         versionName = flutter.versionName
         multiDexEnabled = true // Required for desugaring
     }
