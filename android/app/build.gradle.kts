c6743481d17d236a757bc93cc605a26c891475c7
zamansheikh <zaman6545@gmail.com>
2026-02-17 23:50:53 +0600

    build: Configure Android application with compileSdk 36, desugaring, and release signing, and increment project version to 1.1.8+1.

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -37,7 +37,7 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 11
+        versionCode = 12
         versionName = flutter.versionName
         multiDexEnabled = true // Required for desugaring
     }
