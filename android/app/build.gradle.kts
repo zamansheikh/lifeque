42bc3c136be09fefe9d6ad7a6c51cbb06fdb5708
zamansheikh <zaman6545@gmail.com>
2026-03-05 18:17:56 +0600

    feat: Bump versionCode to 15 and update version number to 1.2.1+1 in build files

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -37,7 +37,7 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 14
+        versionCode = 15
         versionName = flutter.versionName
         multiDexEnabled = true // Required for desugaring
     }
