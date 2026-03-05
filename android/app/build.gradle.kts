2bb8957cd7f7e479a738ff256d62558b5ee1793e
zamansheikh <zaman6545@gmail.com>
2026-03-05 21:06:48 +0600

    feat: Update version to 1.2.2+16 and adjust version code in build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -37,7 +37,7 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 15
+        versionCode = flutter.versionCode
         versionName = flutter.versionName
         multiDexEnabled = true // Required for desugaring
     }
